a19f6449c73352a3abd9b7db08fde44a0e4605b8
Nick <nrwade0@gmail.com>
2020-07-22 09:47:41 -0400

    Remove ulam spiral title for image

--- code/ulam.m
+++ code/ulam.m
@@ -15,5 +15,5 @@ M_prime(isprime(M)) = 1;
 map = [0.2 0.1 0.5; 0.9 1 0]; % [incompleted; completed] RGB triplet
 colormap(map)
 imagesc(M_prime)
-title('Ulam Spiral')
+%title('Ulam Spiral')
 axis off
