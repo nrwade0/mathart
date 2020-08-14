85f06b7bf4eca909c8e8f6f6c1b61603882b8fbe
Nick <nrwade0@gmail.com>
2020-08-14 11:55:35 -0400

    Delete large commented explanation at the end. Placed Wikipedia link in lieu

--- code/rose_curve.m
+++ code/rose_curve.m
@@ -41,26 +41,3 @@ end
 set(gcf, 'Position',  [100, 100, 1200, 800])
 saveas(gcf,'../display/rose_curve.png')
 
-
-
-
-% inputs:
-%   del_theta = del_theta is the discrete step size for discretizing the continuous range of angles from 0 to 2*pi
-%   k = petal coefficient
-%      if k is odd then k is the number of petals
-%      if k is even then k is half the number of petals
-%   amplitude = length of each petal
-% outputs:
-%   a 2D plot from calling this function illustrates an example of trigonometry and 2D Cartesian plotting
-
-%{
-
-where k is a positive integer, has area
-
-
-{\frac  {1}{2}}\int _{{0}}^{{2\pi }}(a\cos(k\theta ))^{2}\,d\theta ={\frac  {a^{2}}{2}}\left(\pi +{\frac  {\sin(4k\pi )}{4k}}\right)={\frac  {\pi a^{2}}{2}}
-if k is even, and
-
-{\frac  {1}{2}}\int _{{0}}^{{\pi }}(a\cos(k\theta ))^{2}\,d\theta ={\frac  {a^{2}}{2}}\left({\frac  {\pi }{2}}+{\frac  {\sin(2k\pi )}{4k}}\right)={\frac  {\pi a^{2}}{4}}
-if k is odd.
-%}
